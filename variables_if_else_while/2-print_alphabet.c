d75fb7f7acc0d55b5e52d7fc8a00dc0ea5de2d7e
abrielleperry <abrielleperry22@icloud.com>
2023-05-25 11:39:37 -0700

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	for (char ch = 'a'; ch <= 'z'; ch++) 
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return 0;
+}
