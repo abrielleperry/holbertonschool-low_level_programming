3ba742ce55113eef28e9300cce2fb83e3e5fdc98
abrielleperry <abrielleperry22@icloud.com>
2023-05-31 14:05:36 -0700

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include <stdlib>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers go here */
+
+/**
+ * mul - returns the multiple of n1 and n2
+ *
+ * @n1: - the first input number
+ *
+ * @n2: the second input number
+ *
+ * Return: Returns the value of the sum of n1 and n2
+ *
+ */
+
+int mul(int n1, int n2)
+{
+	return (n1 * n2);
+
+}
