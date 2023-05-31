d1dac8d53457bffe4ea74f30cb4b45734ae06797
abrielleperry <abrielleperry22@icloud.com>
2023-05-31 13:21:58 -0700

    0

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifndeif _main_h_
+#ifndef _main_h_
 #define _main_h_
 
 #include <limits.h>
@@ -11,7 +11,6 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
