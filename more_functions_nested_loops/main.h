f0c4cd6589888ea1d6e9eaf7ecbaaa42d6d18154
abrielleperry <abrielleperry22@icloud.com>
2023-06-01 07:40:17 -0700

    7

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef _main_h_
+#define _main_h_
+
 int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
@@ -9,3 +12,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+
+#endif
