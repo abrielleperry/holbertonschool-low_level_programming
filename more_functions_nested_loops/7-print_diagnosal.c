93352e04227ef8e00cc08cc4711cd35878f54e4a
abrielleperry <abrielleperry22@icloud.com>
2023-06-01 07:13:58 -0700

    7

---
+++ more_functions_nested_loops/7-print_diagnosal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/* more headers go here */
+
+/**
+ * print_diagnosal - prints the line of length i
+ *
+ * @n: length of line to print
+ *
+ * Return: retuns nothing
+ *
+ */
+
+void print_diagnosal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		if (i < (n - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+
+}
