4a80bb47714563ada7e4a35fe13bc3203e8e9e4f
abrielleperry <abrielleperry22@icloud.com>
2023-06-03 07:37:30 -0700

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/*more headers go here */
+
+/**
+ * print_square - uses hashtag to print square
+ *
+ * @size: size of square
+ *
+ * Return: returns nothing
+ *
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i <= size; i++)
+	{
+		for (j = 0; j <= size; j++)
+		{
+			_putchar('#');
+		}
+		if (i != size - 1)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
