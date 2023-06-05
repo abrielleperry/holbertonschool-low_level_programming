69d2c5dc376cf3710a0eec1c9bf20d2659befc52
abrielleperry <abrielleperry22@icloud.com>
2023-06-05 15:25:42 -0700

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: string input
+ * Return: no return
+ */
+
+void _puts(char *str)
+{
+while (*str)
+_putchar(*str++);
+
+_putchar('\n')
+}
