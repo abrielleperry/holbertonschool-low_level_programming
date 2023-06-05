a70ed7bd052968af0080453c76ebc3318e562957
abrielleperry <abrielleperry22@icloud.com>
2023-06-05 11:29:08 -0700

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 /* more headers go here */
 
 /**
@@ -12,17 +12,17 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < size; i++)
 	{
-		k = (n - i - 1);
-		for (j = 0; j < n; j++)
+		k = (size - i - 1);
+		for (j = 0; j < size; j++)
 		{
 			if (j < k)
 				_putchar(' ');
 			else
 				_putchar('#');
 		}
-		if (i < (n - 1))
+		if (i < (size - 1))
 			_putchar('\n');
 	}
 	_putchar('\n');
