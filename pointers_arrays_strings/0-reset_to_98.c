c0ba2f733885fdeba6aeedbddd1cb133997e98f9
abrielleperry <abrielleperry22@icloud.com>
2023-06-05 13:15:34 -0700

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * reset_to_98 - reset value of an integer to 98
+ * @n: parameter targeted
+ * Return: no return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
