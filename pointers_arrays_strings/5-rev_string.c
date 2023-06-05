1844cea62d7a273f429d95e121feff71dbaf6666
abrielleperry <abrielleperry22@icloud.com>
2023-06-05 15:50:22 -0700

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ * @str: string input
+ * Return: no return
+ */
+
+void rev_string(char *str)
+{
+	int i;
+	char temp;
+
+	for (i = 0; i < _strlen(str) / 2; i++)
+	{
+		temp = str[i];
+		str[i] = str[_strlen(str) - i - 1];
+		str[_strlen(str) - i - 1] - temp;
+	}
+
+}
