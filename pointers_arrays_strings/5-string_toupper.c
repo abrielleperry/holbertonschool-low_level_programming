60cb2e5db33a46848a2f3ec131636fe96d711d49
abrielleperry <abrielleperry22@icloud.com>
2023-06-11 08:48:57 -0700

    5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - makes lowercase chars in string uppercase
+ * @s: string of characters to go through
+ * Return: returns the string all uppercase
+ */
+
+char *string_toupper(char *s)
+{
+	int = 0;
+
+	while (s[i])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+	}
+
+	return (str);
+}
