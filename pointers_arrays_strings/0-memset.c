ab261486e48a5d4d9f93450fcf563cd41b8151c1
abrielleperry <abrielleperry22@icloud.com>
2023-06-11 10:32:06 -0700

    0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - fill memory with a constant byte
+ * @s: memory area
+ * @b: the byte
+ * @n: # of bytes to fill
+ * Return: pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
