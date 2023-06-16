bc0fcccc8515b0c1d0aefd816c359de6dfce84bb
abrielleperry <abrielleperry22@icloud.com>
2023-06-16 10:21:18 -0700

    0

---
+++ recursion/0-puts-recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string followed by a newlist
+ * @s: the string we print
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (!*s)
+	_putchar('\n);
+}
