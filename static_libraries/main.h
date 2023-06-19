539403aec1851922411427535587c8008b0d50dd
abrielleperry <abrielleperry22@icloud.com>
2023-06-19 09:06:56 -0700

    1

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,9 +1,6 @@
 #ifndef _main_h_
 #define _main_h_
 
-#include <stdio.h>
-#include <stdlib.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -24,5 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
+
