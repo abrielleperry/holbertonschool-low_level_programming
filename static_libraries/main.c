cedee1bf59ecef9c058325a27c6749784bb62730
abrielleperry <abrielleperry22@icloud.com>
2023-06-23 08:26:45 -0700

    c

--- static_libraries/main.c
+++ static_libraries/main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main - Entry point
@@ -7,5 +6,6 @@
  */
 int main(void)
 {
+	_puts("\"At the end of the day, my goal was to be the best hacker\"n\t-Kevin Mitnick");
 	return (0);
 }
