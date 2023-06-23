7a2fe06251c3c26099611fe3c57828f51e0c3c9d
abrielleperry <abrielleperry22@icloud.com>
2023-06-23 12:20:38 -0700

    ok

--- static_libraries/3-puts.c
+++ static_libraries/3-puts.c
@@ -1,17 +1,30 @@
 #include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success, returns the character written.
+ * On error, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(STDOUT_FILENO, &c, 1));
+}
 
 /**
  * _puts - a function that prints a string
  * @str: string input
- * Return: string
  */
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
+		if (_putchar(*str) == -1)
+		{
+			return;
+		}
+		str++;
 	}
 	_putchar('\n');
 }
