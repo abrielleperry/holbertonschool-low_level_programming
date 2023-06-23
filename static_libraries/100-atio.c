4e120702e5d15dc319999e47f1a0011b37ce863e
abrielleperry <abrielleperry22@icloud.com>
2023-06-23 11:42:59 -0700

    main

--- static_libraries/100-atio.c
+++ static_libraries/100-atio.c
@@ -5,7 +5,8 @@
  * @s: input
  * Return: Always 0 (Success)
  */
-int _atoi(void)
+int _atoi(char *s)
 {
+	s = s + 1;
 	return (0);
 }
--- static_libraries/3-strcmp.c
+++ static_libraries/3-strcmp.c
@@ -5,14 +5,21 @@
  * @s1: input one
  * @s2: input two
  * Return: Always 0 (Success)
- * */
+ */
 int _strcmp(char *s1, char *s2)
-	int i;
+{
+	int i = 0;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
+		i++;
 	}
-	return (0);
+	if (s1[i] == '\0' && s2[i] == '\0')
+		return (0);
+	else if (s1[i] == '\0')
+		return (-s2[i]);
+	else
+		return (s1[i]);
 }
