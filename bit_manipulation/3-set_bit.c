ddc54e650279db7a580878c3a4c4b173684896dc
abrielleperry <abrielleperry22@icloud.com>
2023-07-16 09:41:32 -0700

    3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * set_bit - sets value of specific bit to 1
+ * @n: input number
+ * @index: digit looking for
+ * Return: Returns 1 or 0 or -1 if failed
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n ^= (1 << index);
+
+	return (1);
+}
