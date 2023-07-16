37b2d2c2cf96655f41402cb5ba1f18611842bd75
abrielleperry <abrielleperry22@icloud.com>
2023-07-16 10:03:45 -0700

    0

---
+++ file_io/README.md
@@ -0,0 +1,3 @@
+#!/bin/bash
+C - File I/O
+
---
+++ file_io/main.h
@@ -0,0 +1,21 @@
+#ifndef main_h
+#define main_h
+
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <math.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
