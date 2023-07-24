3e5777d7c56ecd5b70745ec0228dcad1625ebed5
abrielleperry <abrielleperry22@icloud.com>
2023-07-24 09:17:27 -0700

    add 2

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			if (i == idx)
 			{
-				if (head->next == NULL)							new = add_dnodeint_end(h, n);
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
 				else
 				{
 					new = malloc(sizeof(dlistint_t));
